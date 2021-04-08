2ddcd844ad98e4c2075f74ad144ae9aa8acfe214
Liza <lizzaveta777@gmail.com>
2021-04-08 11:28:40 +0300

    feat: add templates and form in polls

--- server/hospital/polls/views.py
+++ server/hospital/polls/views.py
@@ -1,5 +1,50 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
+from .models import Question, Choice
+
+class IndexView(generic.ListView): #oтображать список объектов(ListView)
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView): #oтображать страницу сведений для опр типа объекта (DetailView)
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def index(request):
-    return HttpResponse("Hello, world. Second try.")
+    #Отображает 5 вопросов, разделяя запятыми,в соответствии с датой
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html',{'question':question})
+
+def results(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html',{'question':question})
+
+
+def vote(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice']) #возвращает идентификатор выбранного варианта в виде строки
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question':question,
+            'error_message':"You didnt select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
